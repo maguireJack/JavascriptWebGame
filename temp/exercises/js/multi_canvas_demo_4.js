c2129e163a40a4002d90a977a421abe552574789
niall mcguinness <niall.mcguinness@dkit.ie>
2020-03-08 18:16:35 +0000

    Added a rotation around canvas origin demo

--- temp/exercises/js/multi_canvas_demo_4.js
+++ temp/exercises/js/multi_canvas_demo_4.js
@@ -6,14 +6,11 @@ var cvs_right = document.getElementById("game-canvas-right")
 var ctx_right = cvs_right.getContext("2d");
 
 /************************************* Left Canvas *************************************/
-let rotationInDegrees = prompt("Enter rotation angle in degrees", "5");
-if (rotationInDegrees == null)
-    rotationInDegrees = 5;
-else
-    rotationInDegrees = Number.parseInt(rotationInDegrees);
 
-//perform -ve rotation around the canvas' origin in the top-left of the drawn canvas (i.e. (0,0))
-ctx_left.rotate(-rotationInDegrees * Math.PI / 180);
+//move the default origin of the canvas to the origin of the rect (200, 200), perform rotation, then reset canvas origin
+ctx_left.translate(200, 200);
+ctx_left.rotate(45 * Math.PI / 180);
+ctx_left.translate(-200, -200);
 
 //draw rect - note the height has been changed to visually differentiate w from h
 ctx_left.beginPath();
@@ -23,12 +20,15 @@ ctx_left.strokeRect(200, 200, 50, 100);
 
 /************************************* Right Canvas *************************************/
 
-//perform +ve rotation around the canvas' origin in the top-left of the drawn canvas (i.e. (0,0))
-ctx_right.rotate(rotationInDegrees * Math.PI / 180);
+//move the default origin of the canvas to the origin of the rect (200, 200), perform rotation, then reset canvas origin
+ctx_right.translate(200, 200);
+ctx_right.rotate(45 * Math.PI / 180);
+ctx_right.translate(-200, -200);
 
 //draw rect - note the height has been changed to visually differentiate w from h
 ctx_right.beginPath();
 ctx_right.lineWidth = 1;
 ctx_right.strokeStyle = "rgb(255, 0, 0)";
-ctx_right.strokeRect(200, 200, 50, 100);
+//notice how we offset (x, y) by subtracting the origin (x, y) values
+ctx_right.strokeRect(200 - 25, 200 - 50, 50, 100);
 
